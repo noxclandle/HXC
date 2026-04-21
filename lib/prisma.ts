8bf87e6f0bddd5a2505cea0eab906ddd517fc439
noxclandle <272180052+noxclandle@users.noreply.github.com>
2026-04-21 22:54:15 +0900

    Fix: Resolve global BigInt serialization and simplify middleware to debug Server Error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+// BigIntのシリアライズ問題を解決
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
