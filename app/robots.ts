0b6cedaf1b6825506a5ec901eb4f3d0c4665dbb4
noxclandle <272180052+noxclandle@users.noreply.github.com>
2026-06-01 12:51:14 +0900

    fix: resolve Search Console redirect error by adding robots.txt and sitemap.ts

---
+++ app/robots.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/admin/',
+        '/hub/',
+        '/api/',
+        '/inventory/',
+        '/shop/',
+        '/library/',
+        '/settings/',
+        '/activate/',
+        '/gate'
+      ],
+    },
+    sitemap: 'https://hxc.hexa-relation.com/sitemap.xml',
+  }
+}
